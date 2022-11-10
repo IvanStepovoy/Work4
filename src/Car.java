a5f30a8bbc503fd73b6d6261f835b77ef889df7f
Kayros23 <istepovoy23@gmail.com>
2022-11-10 04:45:51 +0100

    Done!

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public class Car {
+    String brand;
+    int maxSpeed;
+    int horsePower;
+    int torque;
+
+    public Car ()
+    {
+        brand = "any";
+        maxSpeed = 1;
+        horsePower = 1;
+        torque = 1;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car bmw = new Car();
+
+        System.out.println("Brand is " + bmw.brand);
+        System.out.println("Nax speed is " +bmw.maxSpeed);
+        System.out.println("Horse power is " +bmw.horsePower);
+        System.out.println("Torque is " +bmw.torque);
+
+
+    }
+}
